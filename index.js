eb0d7f89018b064bda84a4dc2e1a1c3dfe558d6f
Thomas <tjb47.dev@gmail.com>
2023-03-27 22:03:27 -0400

    imported modules using (require) added managerQuestions and TeamMember functions to index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./team/Manager');
+const Engineer = require('./team/Engineer');
+const Intern = require('./team/Intern');
+
+
+let managerAnswers = []
+let teamMemberTypeAnswerResults = []
+let teamMembers = []
+let htmlPageContent
+let teamMemberHTML = ""
+let managerHTML = ""
+
+const managerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?',
+  },
+  {
+    type: 'input',
+    name: 'employeeID',
+    message: 'What is your employee ID?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'officeNumber',
+    message: 'What is your office number?',
+  }
+]
+
+const addTeamMember = [
+  {
+      type: 'confirm',
+      name: 'addTeamMember',
+      message: 'Would you like to add an engineer or an intern?'
+    }
+]
